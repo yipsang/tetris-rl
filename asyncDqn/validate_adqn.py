9c1eb2c009ab9c151de82faa95240b1f015e5b3c
Jordan Ruetz <jordan.ruetz@mail.utoronto.ca>
2021-04-20 10:37:49 -0400

    validate adqn

---
+++ asyncDqn/validate_adqn.py
@@ -0,0 +1,76 @@
+import sys
+import torch
+import gym
+from gym_matris import *
+import numpy as np
+
+from value_network import ValueNetwork
+from wrappers import PositionAction
+
+MODEL_PATH = "trained_models/adqn_19_04_2021_21_30/adqn_78700.model"
+
+GPU_ENABLED = False
+RAW_IMAGE = True 
+IS_NOISY = True
+
+TRIAL_EPISODES = 100
+MAX_STEP = 500
+
+
+def main():
+    env = gym.make("matris-v0", render=False, timestep=0.05)
+    env = PositionAction(
+        env, handcrafted_features=True, with_next_tetromino=True
+    )
+
+    device = "cpu"
+    if GPU_ENABLED:
+        device = "cuda"
+
+    print("loading model...")
+    value_net = ValueNetwork(
+        env.observation_space.shape[0], layers_size=[128, 256]).to(device)
+    value_net.load_state_dict(
+        torch.load(MODEL_PATH, map_location=torch.device(device))
+    )
+
+    value_net.eval()
+
+    steps = []
+    rewards = []
+    for i in range(TRIAL_EPISODES):
+        sys.stdout.write("\033[K")
+        print(
+            "running tests: {}%".format(round((i + 1) / TRIAL_EPISODES, 2) * 100),
+            end="\r",
+        )
+        step_count = 0
+        total_reward = 0
+        state, actions_and_next_states, reward, done, info = env.reset()
+        while step_count < MAX_STEP and not done:
+            step_count += 1
+            actions, next_states = zip(*actions_and_next_states)
+            next_states = torch.from_numpy(np.array(next_states)).float().to(device)
+            state_values = value_net(next_states)
+
+            action_idx = np.argmax(state_values.cpu().data.numpy())
+            action = actions[action_idx]
+            state = next_states[action_idx]
+
+            actions_and_next_states, reward, done, info = env.step(action)
+            total_reward += reward
+
+        steps.append(step_count)
+        rewards.append(total_reward)
+
+    print("")
+    steps = np.array(steps)
+    rewards = np.array(rewards)
+    print("Avg reward:", np.mean(rewards), "| std:", np.std(rewards))
+    print("Avg steps:", np.mean(steps), "| std:", np.std(steps))
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
