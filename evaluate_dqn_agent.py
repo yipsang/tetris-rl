1a9bc073ff41ae9480e44195bb0331de4ebfaf29
Yip Sang Leung <thirtythousandollar@gmail.com>
2021-04-20 18:28:37 -0400

    Upate evaluate_dqn_agent to support CNN

--- evaluate_dqn_agent.py
+++ evaluate_dqn_agent.py
@@ -1,34 +1,52 @@
-from pickle import FALSE, TRUE
 import torch
 import gym
 from gym_matris import *
 import numpy as np
 import cv2
 
-from value_network import ValueNetwork
+from value_network import ConvValueNetwork, ValueNetwork
 from wrappers import PositionAction
 
 MODEL_PATH = "trained_models/dqn_27_03_2021_01_21/dqn_6500_10000.model"
 GPU_ENABLED = False
 RECORD = True
 VIDEO_PATH = "dqn.mp4"
+RAW_IMAGE = False
+IS_NOISY = True
 
 MAX_STEP = 10000
 
 
 def main():
     env = gym.make("matris-v0", render=True, timestep=0.05)
-    env = PositionAction(env, handcrafted_features=True)
+    env = PositionAction(
+        env, handcrafted_features=not RAW_IMAGE, with_next_tetromino=True
+    )
 
     device = "cpu"
     if GPU_ENABLED:
         device = "cuda"
 
     print("loading model...")
-    value_net = ValueNetwork(env.observation_space.shape[0], layers_size=[128, 256]).to(
-        device
-    )
-    value_net.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device(device)))
+    if not RAW_IMAGE:
+        value_net = ValueNetwork(
+            env.observation_space.shape[0], layers_size=[128, 256], is_noisy=IS_NOISY
+        ).to(device)
+        value_net.load_state_dict(
+            torch.load(MODEL_PATH, map_location=torch.device(device))
+        )
+    else:
+        input_shape = env.observation_space.shape
+        value_net = ConvValueNetwork(
+            input_shape[:2],
+            input_shape[-1],
+            layers_config=[(2, 1, 0, 32), (2, 2, 1, 32), (1, 1, 0, 32)],
+            linear_layer_size=256,
+            is_noisy=True,
+        ).to(device)
+        value_net.load_state_dict(
+            torch.load(MODEL_PATH, map_location=torch.device(device))
+        )
 
     state, actions_and_next_states, reward, done, info = env.reset()
 
