d8d8e13eabee1321c54e941725b788d7b26b04d0
Yip Sang Leung <thirtythousandollar@gmail.com>
2021-04-20 20:31:37 -0400

    Update train_tetris_dqn_agent default config

--- train_tetris_dqn_agent.py
+++ train_tetris_dqn_agent.py
@@ -8,20 +8,23 @@ logging.basicConfig(level=logging.INFO)
 def main():
     trainer = TetrisDQNAgentTrainer(
         layers_size=[128, 256],
-        buffer_size=100000,
-        batch_size=64,
-        gamma=0.99,
+        buffer_size=90000,
+        batch_size=256,
+        gamma=1,
         lr=1e-4,
         freeze_step=1,
         gpu=False,
         episodes=10000,
         render=False,
         random_action_episodes=100,
-        episolon_decay_episodes=750,
+        episolon_decay_episodes=5000,
         start_eps=1,
-        end_eps=0.01,
+        end_eps=1e-4,
         max_episode_step=1000,
-        save_every=10,
+        handcrafted_features=True,
+        conv_layers_config=[(2, 1, 0, 32), (2, 2, 1, 32), (1, 1, 0, 32)],
+        conv_linear_size=256,
+        is_noisy=True,
     )
     losess, rewards = trainer.start()
 
